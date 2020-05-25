4f5917147e9345d304dd3ef6c7b72b3bd07469a8
liky12 <gayoung0703@naver.com>
2020-05-25 18:33:03 +0900

    20200525수정

--- js/myscript.js
+++ js/myscript.js
@@ -2,3 +2,13 @@ $(".tabmenu>li").on("click",function(){
     $(this).siblings().removeClass("active")
     $(this).addClass("active")
 })
+
+$(".depth1 > li >a").on("mouseover focus",function(){
+    $(this).next().stop().fadeIn(500)
+    $(this).parent().siblings().children(".depthbox2").stop().fadeOut(100)
+})
+
+// $("#header .openMobgnb").on("click", function(){
+//         $(this).parents("#header").toggleClass("on")
+//         $(this).find("span").css({background:"rgba(1, 1, 1, 0.233)"})
+// })
