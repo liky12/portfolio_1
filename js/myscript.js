5442018dc87aaac20faecc0bdfd7b74d922a8b5d
liky12 <gayoung0703@naver.com>
2020-05-26 18:29:47 +0900

    20200526

--- js/myscript.js
+++ js/myscript.js
@@ -4,11 +4,10 @@ $(".tabmenu>li").on("click",function(){
 })
 
 $(".depth1 > li >a").on("mouseover focus",function(){
-    $(this).next().stop().fadeIn(500)
-    $(this).parent().siblings().children(".depthbox2").stop().fadeOut(100)
+    $(this).next().stop().slideDown(500)
+    $(this).parent().siblings().children(".depthbox2").stop().fadeOut(200)
 })
 
-// $("#header .openMobgnb").on("click", function(){
-//         $(this).parents("#header").toggleClass("on")
-//         $(this).find("span").css({background:"rgba(1, 1, 1, 0.233)"})
-// })
+$("#header .openMobgnb").on("click", function(){
+        $(this).parents("#header").toggleClass("on")
+})
