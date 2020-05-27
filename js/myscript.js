364d7880b0ba47573f4e591ea4f41ce994312df3
liky12 <gayoung0703@naver.com>
2020-05-27 18:45:12 +0900

    20200527

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,13 @@
-$(".tabmenu>li").on("click",function(){
-    $(this).siblings().removeClass("active")
-    $(this).addClass("active")
-})
+// $(".tabmenu>li").on("click",function(){
+//     $(this).siblings().removeClass("active")
+//     $(this).addClass("active")
+// })
 
-$(".depth1 > li >a").on("mouseover focus",function(){
-    $(this).next().stop().slideDown(500)
-    $(this).parent().siblings().children(".depthbox2").stop().fadeOut(200)
-})
+// $(".depth1 > li >a").on("mouseover focus",function(){
+//     $(this).next().stop().slideDown(500)
+//     $(this).parent().siblings().children(".depthbox2").stop().fadeOut(200)
+// })
 
-$("#header .openMobgnb").on("click", function(){
-        $(this).parents("#header").toggleClass("on")
-})
+// $("#header .openMobgnb").on("click", function(){
+//         $(this).parents("#header").toggleClass("on")
+// })
