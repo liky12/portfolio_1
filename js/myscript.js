2b5ed6907e75d596e89993c2991868967399daa8
liky12 <gayoung0703@naver.com>
2020-05-29 18:22:43 +0900

    20200529 수정1

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,13 @@
-// $(".tabmenu>li").on("click",function(){
-//     $(this).siblings().removeClass("active")
-//     $(this).addClass("active")
-// })
+$(".depth1 > li >a").on("click" ,function(){
+    $(this).next().stop().slideDown(400)
+    $(this).parent().siblings().children(".depthbox2").stop().fadeOut(0)
+})
 
-// $(".depth1 > li >a").on("mouseover focus",function(){
-//     $(this).next().stop().slideDown(500)
-//     $(this).parent().siblings().children(".depthbox2").stop().fadeOut(200)
-// })
+
+
+$(".depthbox2").on("mouseleave" , function(){
+    $(this).stop().slideUp(400)
+})
 
 // $("#header .openMobgnb").on("click", function(){
 //         $(this).parents("#header").toggleClass("on")
