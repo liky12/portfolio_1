1c1756c77ca6e12a1dbe9c49be45f91d126d91af
liky12 <gayoung0703@naver.com>
2020-06-01 14:20:49 +0900

    20200531 수정

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,13 @@
-$(".depth1 > li >a").on("click" ,function(){
-    $(this).next().stop().slideDown(400)
-    $(this).parent().siblings().children(".depthbox2").stop().fadeOut(0)
-})
+// $(".depth1 > li >a").on("click" ,function(){
+//     $(this).next().stop().slideDown(400)
+//     $(this).parent().siblings().children(".depthbox2").stop().fadeOut(0)
+// })
 
 
 
-$(".depthbox2").on("mouseleave" , function(){
-    $(this).stop().slideUp(400)
-})
+// $(".depthbox2").on("mouseleave" , function(){
+//     $(this).stop().slideUp(400)
+// })
 
 // $("#header .openMobgnb").on("click", function(){
 //         $(this).parents("#header").toggleClass("on")
