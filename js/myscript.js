de3c6a3220bc7a495708369d508d479cab81ab9e
liky12 <gayoung0703@naver.com>
2020-06-02 18:34:35 +0900

    20200602 수정

--- js/myscript.js
+++ js/myscript.js
@@ -38,7 +38,7 @@ $(".sdgimg").slick({
     autoplay:true,//기본값 false
     autoplaySpeed:2000,//기본값3000 슬라이드 시간
     dots:true,//기본값 false 슬라이드 번호 버튼
-    speed:400, //슬라이드 이동 시간
+    speed:600, //슬라이드 이동 시간
     slideToShow:1,//첫화면에 보여지는 슬라이드 수
     slideToScroll:1,//넘어가는 슬라이드 수
     pauseOnHover:false,//  슬라이드 멈춤 여부
@@ -50,4 +50,21 @@ $(".sdgimg").slick({
     arrows:true,//기본값true,슬라이드 방향 버튼
     prevArrow: '<button class="prevArrow marrow"><i class="fas fa-angle-left"></i></button>',
     nextArrow: '<button class="nextArrow marrow"><i class="fas fa-angle-right"></i></button>',
+})
+
+
+$(".tab_slide").slick({
+    autoplay:false,//기본값 false
+    autoplaySpeed:2000,//기본값3000 슬라이드 시간
+    dots:true,//기본값 false 슬라이드 번호 버튼
+    speed:200, //슬라이드 이동 시간
+    slideToShow:1,//첫화면에 보여지는 슬라이드 수
+    slideToScroll:1,//넘어가는 슬라이드 수
+    pauseOnHover:true,//  슬라이드 멈춤 여부
+    pauseOnDotsHover:true,//슬라이드 버튼 위에 마우스오버시 멈춤
+    pauseOnFocus:false,//동그라미 버븐클릭시 자동실행 멈춤여부
+    cssEase:"linear",//슬라이드 가속도
+    draggable:false,//마우스 드래그시 움직임 여부
+    fade:false,//넘어가는 방향(오->왼), true:제자리아웃온 효과
+    arrows:false,//기본값true,슬라이드 방향 버튼
 })
