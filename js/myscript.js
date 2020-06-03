60891b6908af3987ae6f6b915d8d82e2720e43c4
liky12 <gayoung0703@naver.com>
2020-06-03 14:39:40 +0900

    20200602 수정

--- js/myscript.js
+++ js/myscript.js
@@ -13,30 +13,10 @@ $("header").on("mouseleave" , function(){
 // })
 
 
-// $(".tab_menu > a").eq(0).on("click",function(){
-//     $(this).css("color","black").css("border-bottom","1px solid #000")
-//     $(this).siblings().css("color","white").css("border-bottom","none")
-//     $(this).parent().siblings().removeClass("on")
-//     $(this).parent().siblings(".tab_mov").addClass("on")
-// })
-// $(".tab_menu > a").eq(1).on("click",function(){
-//     $(this).css("color","black").css("border-bottom","1px solid #000")
-//     $(this).siblings().css("color","white").css("border-bottom","none")
-//     $(this).parent().siblings().removeClass("on")
-//     $(this).parent().siblings(".tab_product").addClass("on")
-// })
-// $(".tab_menu >a").eq(2).on("click",function(){
-//     $(this).css("color","red").css("border-bottom","1px solid red")
-//     $(this).siblings().css("color","white").css("border-bottom","none")
-//     $(this).parent().siblings().removeClass("on")
-//     $(this).parent().siblings(".tab_product2").addClass("on")
-// })
-
-
 
 $(".sdgimg").slick({
     autoplay:true,//기본값 false
-    autoplaySpeed:2000,//기본값3000 슬라이드 시간
+    autoplaySpeed:5000,//기본값3000 슬라이드 시간
     dots:true,//기본값 false 슬라이드 번호 버튼
     speed:600, //슬라이드 이동 시간
     slideToShow:1,//첫화면에 보여지는 슬라이드 수
@@ -48,8 +28,8 @@ $(".sdgimg").slick({
     draggable:true,//마우스 드래그시 움직임 여부
     fade:false,//넘어가는 방향(오->왼), true:제자리아웃온 효과
     arrows:true,//기본값true,슬라이드 방향 버튼
-    prevArrow: '<button class="prevArrow marrow"><i class="fas fa-angle-left"></i></button>',
-    nextArrow: '<button class="nextArrow marrow"><i class="fas fa-angle-right"></i></button>',
+    prevArrow: '<button class="prevArrow marrow"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow: '<button class="nextArrow marrow"><i class="fas fa-chevron-right"></i></button>',
 })
 
 
@@ -61,10 +41,17 @@ $(".tab_slide").slick({
     slideToShow:1,//첫화면에 보여지는 슬라이드 수
     slideToScroll:1,//넘어가는 슬라이드 수
     pauseOnHover:true,//  슬라이드 멈춤 여부
-    pauseOnDotsHover:true,//슬라이드 버튼 위에 마우스오버시 멈춤
+    pauseOnDotsHover:false,//슬라이드 버튼 위에 마우스오버시 멈춤
     pauseOnFocus:false,//동그라미 버븐클릭시 자동실행 멈춤여부
     cssEase:"linear",//슬라이드 가속도
     draggable:false,//마우스 드래그시 움직임 여부
     fade:false,//넘어가는 방향(오->왼), true:제자리아웃온 효과
     arrows:false,//기본값true,슬라이드 방향 버튼
+})
+
+
+
+
+$(".topbt").on("click",function(){
+    $("body,html").animate({scrollTop:"0px"},600);
 })
